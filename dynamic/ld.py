a4ab924cbfd1dd9f09588b0d37f60fdf6249a1e1
uni <uni.xu@moca-tech.net>
2017-09-13 11:32:56 +0800

    添加代码注释，方便阅读

--- dynamic/ld.py
+++ dynamic/ld.py
@@ -3,12 +3,19 @@ LD算法（Levenshtein Distance）又成为编辑距离算法（Edit Distance）
 它是以字符串A通过插入字符、删除字符、替换字符变成另一个字符串B
 操作过程的次数表示两个字符串的差异。
 
+
+to do list:
+1.添加可视化高亮动态展示
+
 """
 import numpy as np
 class LD(object):
     def __init__(self):
         pass
     def ld(self,a,b):
+        """
+        统计编辑距离
+        """
         len_a = len(a)
         len_b = len(b)
         if min(len_a,len_b) == 0:
@@ -20,6 +27,14 @@ class LD(object):
             return min(ld_tuple) + 1
 
     def ld_and_mat(self,a,b):
+        """
+        统计编辑距离并生成编辑矩阵
+        编辑矩阵的数值是对应文本的最小编辑距离
+
+        利用动态规划，如果当前对应元素相等，
+        编辑距离等于a[:a.length-1]和b[:b.length-1]对应的编辑距离
+        如果当前对应元素不相等，则对应三种情况中的最小值+1
+        """
         len_a = len(a)
         len_b = len(b)
         if min(len_a,len_b) == 0:
@@ -33,24 +48,27 @@ class LD(object):
                         self.ld_mat[0][i] = i
             return max(len_a,len_b)
         if a[len_a-1] == b[len_b-1]:
-            ## 如果对应两个字母相同，则对应位置为
             if self.ld_mat[len_a-1][len_b-1] == 0:
                 self.ld_mat[len_a-1][len_b-1] = self.ld_and_mat(a[:len_a-1],b[:len_b-1])
         else:
             ld_tuple= self.ld_and_mat(a[:len_a-1],b[:len_b-1]),self.ld_and_mat(a[:len_a],b[:len_b-1]),self.ld_and_mat(a[:len_a-1],b[:len_b])
             if self.ld_mat[len_a-1][len_b-1] == 0:
                 self.ld_mat[len_a-1][len_b-1] = min(ld_tuple) + 1
-            
         return self.ld_mat[len_a-1][len_b-1]
 
     def print_diff(self,a,b): 
+        """
+        根据编辑矩阵生成差异文本
+
+        编辑矩阵优先考虑左移，如果左边不为零且小于左上方
+        当移动到第一行或者第一列时，特殊考虑
+        """
         index_mat_a = len(a) - 1
         index_mat_b = len(b) - 1
         diff_a = ""
         diff_b = ""
         head_flag = 0
         while index_mat_a > 0 and index_mat_b > 0:
-            ##如果对应元素相等，则
             if self.ld_mat[index_mat_a][index_mat_b-1] != 0 and self.ld_mat[index_mat_a][index_mat_b-1] <= self.ld_mat[index_mat_a-1][index_mat_b-1]:
                 diff_a = "_" + diff_a
                 diff_b = b[index_mat_b] + diff_b
@@ -62,7 +80,7 @@ class LD(object):
                 index_mat_a = index_mat_a - 1
                 index_mat_b = index_mat_b - 1
             """
-            逻辑复杂且不严谨，直接观察矩阵便可
+            逻辑复杂且不严谨，直接观察矩阵规律，见方法注释
             # if self.ld_mat[index_mat_a-1][index_mat_b] == 0:
             #     diff_a = a[index_mat_a] + diff_a
             #     diff_b = b[index_mat_b] + diff_b
@@ -130,15 +148,16 @@ class LD(object):
                     index_mat_b = index_mat_b
         print(diff_a,diff_b)
 
-
     def ld_and_print_diff(self,a,b):
+        """
+        调整a,b顺序，保证矩阵为宽矩阵
+        """
         if len(a) > len(b):
             a,b = b,a
         self.ld_mat = np.mat(np.zeros(len(a)*len(b)).reshape(len(a),len(b))).tolist()
         print(self.ld_and_mat(a,b))
         print(self.ld_mat)
         self.print_diff(a,b)
-
 
 if __name__ == '__main__':
     ld_object = LD()
