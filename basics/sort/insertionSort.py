963a40c7add73af9e8e041b48127e475527d6a4f
uni.xu <691420417@qq.com>
2018-04-26 13:35:19 +0800

    add some basic algorithms

--- basics/sort/insertionSort.py
+++ basics/sort/insertionSort.py
@@ -11,3 +11,19 @@ def insertionSort(arr):
 arr = [12, 11, 13, 5, 6]
 insertionSort(arr)
 print(arr)
+
+def recursive_insert_sort(arr,i):
+    if i == len(arr):
+        return
+    j = i - 1
+    key = arr[i]
+    while j >=0 and arr[j] > key:
+        arr[j+1] = arr[j]
+        j -= 1
+    arr[j + 1] = key
+    recursive_insert_sort(arr,i+1)
+arr = [12, 11, 13, 5, 6]
+recursive_insert_sort(arr,1)
+print(arr)
+
+
